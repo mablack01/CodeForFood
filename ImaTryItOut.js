5cd36020f4d9ae20b91b93642edfe93a79ee36b3
ibrahimshaheen <ishaheen1995@gmail.com>
2018-04-11 14:55:55 -0400

    template

---
+++ ImaTryItOut.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	  host     : 'localhost',
+	  user     : '< MySQL username >',
+	  password : '< MySQL password >',
+	  database : '<your database name>'
+});
+
+connection.connect();
+
+connection.query('SELECT * from < table name >', function(err, rows, fields) {
+	  if (!err)
+		    console.log('The solution is: ', rows);
+	  else
+		    console.log('Error while performing Query.');
+});
+
+connection.end();
