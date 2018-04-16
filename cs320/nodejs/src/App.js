983882ee52a87d1af9efa1629c197f59c3678012
sky <skywu9@gmail.com>
2018-04-16 19:45:42 -0400

    Testing buttons

--- cs320/nodejs/src/App.js
+++ cs320/nodejs/src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Code4Food</h1>
-        </header>
-      </div>
-    );
-  }
+
+	constructor() {
+		super()
+		this.state = {
+			username: 'yo'
+		}
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick() {
+		console.log("Success")
+		axios.get('https://api.github.com/users/maecapozzi')
+		.then(response => this.setState({username:response.data.name}))
+	}
+
+
+
+	render() {
+		return (
+	 
+			<div className='button_container'>
+				<button className='button' onClick={this.handleClick}>
+					Click Me
+				</button>
+				<p>{this.state.username}</p>
+			</div>
+
+		);
+	}
 }
+
+
 
 export default App;
