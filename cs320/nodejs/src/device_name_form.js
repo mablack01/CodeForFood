5793573db80893ca916935ea12c62d6b30357859
Dan <danielmacdon@umass.edu>
2018-04-17 16:29:00 -0400

    Simpler React Form

--- cs320/nodejs/src/device_name_form.js
+++ cs320/nodejs/src/device_name_form.js
@@ -1,4 +1,4 @@
-class RenameDeviceForm = React.createClass({
+var RenameDeviceForm = React.createClass({
 	getInitialState: function() {
 		return {
 			device_id: undefined,
@@ -6,34 +6,23 @@ class RenameDeviceForm = React.createClass({
 		}
 	}
 }
-	handleSubmit: function(e) {
-		e.preventDefault();
-
-		var device_id = this.state.device_id;
-		var device_name  = this.state.device_name.trim();
+	handleChange: function(event) {
+		this.setState({device_name: event.target.value});
 	}
 
-	setValue: function(field, event){
-		var object = {};
-		object[field] = event.target.value;
-		this.setState(object);
+	_onChange: function(event){
+		event.preventDefault();
+		console.log("Device Name:" + this.state.device_name);
 	}
 
 	render: function(){
 		return (
-			<form className="renameDeviceForm" onSubmit={this.handleSubmit}>
+			<form method="post">
 				<h2>Rename Device</h2>
-
-				<TextInput
-					value={this.state.device_name}
-					uniqueName="device_name"
-					text="Device Name"
-					textArea={false}
-					required={true}
-					maxCharacters={10}
-					onChange={this.setValue.bind(this, 'device_name')} >
-				<input type="submit" value="Submit"/>
+				<input type="text" onChange="{this.handleChange}"/>
+				<button onClick={this._onChange}Submit</button>
 			</form>
 
 		)
 	}
+	React.render(<RenameDeviceForm />, docuement.getElementById('container'));
