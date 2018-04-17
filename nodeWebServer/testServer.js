9cf9e20873cc11f2af2e467b4b74dafc1231a36a
ibrahimshaheen <ishaheen1995@gmail.com>
2018-04-17 17:02:09 -0400

    Basic node webserver (without react) added

---
+++ nodeWebServer/testServer.js
@@ -0,0 +1,26 @@
+mysql = require('mysql');
+
+var con = mysql.createConnection({
+	        host: "localhost",
+	        user: "root",
+	        password: "toor",
+	        database: "Devices"
+});
+
+con.connect(function(err) {
+	        if (err) throw err;
+	        con.query("SELECT * FROM Device", function (err, result, fields) {
+			                if (err) throw err;
+			                        console.log("\n");
+			                        console.log("##### Hi I'm the node server, here is the data from the database");
+			                        console.log("\n");
+			                        console.log(result);
+			                });
+});
+
+http = require('http');
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end('Hello Universe! This is the CS320 project for team Code4Food!');
+}).listen(8080);
