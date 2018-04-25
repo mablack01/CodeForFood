34f8a7c022614eb3ef78899c3743c8cee4a2fed4
ibrahimshaheen <ishaheen1995@gmail.com>
2018-04-25 13:50:53 -0400

    Formatting changes

--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ const endpoints = require('./endpoints')
 const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.json())
-app.post('/createUser', (req, res) => { endpoints
+app.post('/createUser', function(req, res) { endpoints
 	.createUser({
 		deviceName: req.body.deviceName,
 		deviceID: req.body.deviceID
 	})
-	.then(() => res.sendStatus(200))
+	.then(function(){ res.sendStatus(200)})
 })
 
 app.get('/showDatabase', function (req, res) {
@@ -39,6 +39,6 @@ app.get('/showDatabase', function (req, res) {
 	});
 })
 
-app.listen(7554, () => {
+app.listen(7554, function() {
 	console.log('Server running on http://localhost:7554')
 })
