83584a01a32bafa5505093da7f96866ea1057327
Dan <danielmacdon@umass.edu>
2018-04-27 16:15:30 -0400

    branch fix

--- endpoints/connection.js
+++
@@ -1,9 +0,0 @@
-mysql = require('mysql');
-
-var testDB = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "toor",
-	database: "Devices"
-});
-
--- endpoints/device.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	renameDevice ({device_name}) {
-		console.log('New Name: ${device_name}')
-		return Promise.resolve()
-	}
-}
