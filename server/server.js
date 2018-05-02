81805c8df652faa85ee2d6af2b0bdfef3689d197
Blumbus <blakeamiot@gmail.com>
2018-05-02 17:54:57 -0400

    Disabling settings input

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const dbconnect = require('./dbconnect.js')
 const session = require('express-session');
 const cookieParser = require('cookie-parser')
 
