0baa76a15f294cacf37d55436e3650c90f161f0c
Blumbus <blakeamiot@gmail.com>
2018-05-03 01:02:12 -0400

    Alerts

--- server/server.js
+++ server/server.js
@@ -69,7 +69,8 @@ app.get('/', function(req,res){
 
 app.get('/home', requireLogin, function(req, res){
   
-
+  tenantID = req.session.userID;
+  console.log(tenantID);
   var serialNum, companyName, fullModel, totalSizeTiB, freeSizeTiB;
   var deviceInfo = new Array();
 
@@ -77,21 +78,23 @@ app.get('/home', requireLogin, function(req, res){
    MongoClient.connect(dburl, function (err, client) {
         if (err) { console.log("error in connection to Devicedata")}
       var db = client.db('Devicedata');
-        db.collection('umass_export_25', function (err, collection) {
+        db.collection('fulldata', function (err, collection) {
           
 
-          collection.find({}).limit(10).toArray(function(err, result) {
+          collection.find({"authorized.tenants": tenantID}).limit(20).toArray(function(err, result) {
               if(err) throw err; 
 
               for (let i = 0; i < result.length; i++){
-                var device = {
-                  serialNum : result[i].serialNumberInserv,
-                  companyName :result[i].system.companyName,
-                  fullModel : result[i].system.fullModel,
-                  totalSizeTiB : result[i].capacity.total.sizeTiB,
-                  freeSizeTiB : result[i].capacity.total.freeTiB
+                if (result[i].performance.portBandwidthData) {
+                  var device = {
+                    serialNum : result[i].serialNumberInserv,
+                    companyName :result[i].system.companyName,
+                    fullModel : result[i].system.fullModel,
+                    totalSizeTiB : result[i].capacity.total.sizeTiB,
+                    freeSizeTiB : result[i].capacity.total.freeTiB
+                  }
+                  deviceInfo.push(device);
                 }
-                deviceInfo.push(device);
               }
               
               res.render('home', {device: deviceInfo});
@@ -127,6 +130,7 @@ app.post('/login', function(req, res) {
                   else if(result.password == password){
                     userID= result.uid
                     req.session.user = username;
+                    req.session.userID = userID;
                     res.redirect('/home')
                 } 
               // Else redirect to login page
@@ -160,9 +164,7 @@ app.get('/error', function(req, res){
   res.redirect('/');
 })
 
-
-
-app.post('/viewAlert', requireLogin, function(req, res){
+app.post('/triggeredAlerts', requireLogin, function(req, res){
   var id = req.body.devID;
   var devID = id.substring(3, id.length);
   var freeTiB, totalDiskCount, diskState, readMax, readMin, readAvg, writeMax, writeMin,  writeAvg;
@@ -171,7 +173,136 @@ app.post('/viewAlert', requireLogin, function(req, res){
   MongoClient.connect("mongodb+srv://cs320:root@cluster0-9bmfr.mongodb.net/test", function (err, client) {
           if (err) { console.log("error in connection to User")}
         var db = client.db('Devicedata');
-          db.collection('umass_export_25', function (err, collection) {
+          db.collection('fulldata', function (err, collection) {
+          
+          // get the required info of the deivce
+
+              collection.findOne({serialNumberInserv: devID}, function(err, result) {
+                  if(err) throw err;
+                   deviceInfo =
+                    {
+                    freeTiB : result.capacity.total.freeTiB,
+                    totalDiskCount: result.disks.total.diskCount,
+                    diskState: result.disks.state,
+                    readMax: result.performance.portBandwidthData.read.iopsMax,
+                    readMin: result.performance.portBandwidthData.read.iopsMin,
+                    readAvg: result.performance.portBandwidthData.read.iopsAvg,
+                    writeMax: result.performance.portBandwidthData.write.iopsMax,
+                    writeMin: result.performance.portBandwidthData.write.iopsMin,
+                    writeAvg: result.performance.portBandwidthData.write.iopsAvg
+                    }
+             
+              }); 
+                  
+
+          });//closing getCollection 
+
+          // get the info of threshold
+          db.collection('threshold', function (err, collection) {
+          
+              collection.findOne({serialNumberInserv: devID}, function(err, result) {
+                  if(err) throw err;
+                  thresholdInfo = 
+                  {
+                    freeTiB : [25, 'true'],
+                    totalDiskCount: [25, 'true'],
+                    diskState: [25, 'true'],
+                    readMax: [25, 'true'],
+                    readMin: [25, 'true'],
+                    readAvg: [25, 'true'],
+                    writeMax: [25, 'true'],
+                    writeMin: [25, 'true'],
+                    writeAvg: [25, 'true']
+                  }
+                  if (result) {
+                    thresholdInfo = 
+                    {
+                      freeTiB : result.capacity.total.freeTiB,
+                      totalDiskCount: result.disks.total.diskCountNormal,
+                      diskState: result.disks.state,
+                      readMax: result.performance.portBandwidthData.read.iopsMax,
+                      readMin: result.performance.portBandwidthData.read.iopsMin,
+                      readAvg: result.performance.portBandwidthData.read.iopsAvg,
+                      writeMax: result.performance.portBandwidthData.write.iopsMax,
+                      writeMin: result.performance.portBandwidthData.write.iopsMin,
+                      writeAvg: result.performance.portBandwidthData.write.iopsAvg
+                    }
+                  }
+                  
+                  var alertInfo = {
+                    freeTiB: false,
+                    totalDiskCount: false,
+                    readMax: false,
+                    readMin: false,
+                    readAvg:false,
+                    writeMax:false,
+                    writeMin:false,
+                    writeAvg:false
+                  }
+                  if (thresholdInfo.freeTiB[1] == 'true') {
+                    if(deviceInfo.freeTiB < thresholdInfo.freeTiB[0]){
+                      alertInfo.freeTiB = true;
+                    }
+                  }
+                  if (thresholdInfo.totalDiskCount[1] == 'true') {
+                    if(deviceInfo.totalDiskCount < thresholdInfo.totalDiskCount[0]){
+                      alertInfo.totalDiskCount = true;
+                    }
+                  }
+                  if (thresholdInfo.readMax[1] == 'true') {
+                    if(deviceInfo.readMax < thresholdInfo.readMax[0]){
+                      alertInfo.readMax = true;
+                    }
+                  }
+                  if (thresholdInfo.readMin[1] == 'true') {
+                    if(deviceInfo.readMin < thresholdInfo.readMin[0]){
+                      alertInfo.readMin = true;
+                    }
+                  }
+                  if (thresholdInfo.readAvg[1] == 'true') {
+                    if(deviceInfo.readAvg < thresholdInfo.readAvg[0]){
+                      alertInfo.readAvg = true;
+                    }
+                  }
+                  if (thresholdInfo.writeMax[1] == 'true') {
+                    if(deviceInfo.writeMax < thresholdInfo.writeMax[0]){
+                      alertInfo.writeMax = true;
+                    }
+                  }
+                  if (thresholdInfo.writeMin[1] == 'true') {
+                    if(deviceInfo.writeMin < thresholdInfo.writeMin[0]){
+                      alertInfo.writeMin = true;
+                    }
+                  }
+                  if (thresholdInfo.writeAvg[1] == 'true') {
+                    if(deviceInfo.writeAvg < thresholdInfo.writeAvg[0]){
+                      alertInfo.writeAvg = true;
+                    }
+                  }
+                  res.send({alert: alertInfo});
+
+              }); 
+          });
+      
+
+             //send them to frontend
+
+          client.close();            
+    });//closing connect
+
+})
+
+
+app.post('/viewSettings', requireLogin, function(req, res){
+  var id = req.body.devID;
+  var devID = id.substring(3, id.length);
+  var freeTiB, totalDiskCount, diskState, readMax, readMin, readAvg, writeMax, writeMin,  writeAvg;
+  var deviceInfo, thresholdInfo;
+
+  MongoClient.connect("mongodb+srv://cs320:root@cluster0-9bmfr.mongodb.net/test", function (err, client) {
+          if (err) { console.log("error in connection to User")}
+        var db = client.db('Devicedata');
+          db.collection('fulldata', function (err, collection) {
           
           // get the required info of the deivce
 
